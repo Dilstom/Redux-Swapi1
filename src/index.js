d7b90d47b723a9182682a3b44a00173848c0532a
Dil Sombayeva <dilyasom@gmail.com>
2019-06-23 15:51:43 -0700

    type

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducers';
 // needed dependancies
-import { applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-import { logger } from 'redux-logger';
-import { rootReducer } from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+// import { rootReducer } from './reducers';
 
 const store = createStore(
  rootReducer,
